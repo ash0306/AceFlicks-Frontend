5c89e8896dfe769f4dc31d413a27408432a81ccc
unknown <aswathi259@gmail.com>
2024-08-02 23:16:15 +0530

    Updated YAML file

--- src/utilities/axiosConfig.js
+++ src/utilities/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "https://acetickets.azurewebsites.net/api/", // Replace with your backend URL
+  baseURL: "https://acetickets.azurewebsites.net/api", // Replace with your backend URL
   timeout: 60000, // Request timeout in milliseconds
   headers: {
     "Content-Type": "application/json",
