2e1596f347a9855cd6f22fd1ecdbf4d38c1bcc5f
unknown <aswathi259@gmail.com>
2024-08-02 14:20:13 +0530

    Minor change

--- src/AdminComponents/TestComponent.jsx
+++ src/AdminComponents/TestComponent.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import AddMovieComponent from './FormComponents/AddMovieComponent'
 import AddShowtimeComponent from './FormComponents/AddShowtimeComponent'
 import AddTheatreComponent from './FormComponents/AddTheatreComponent'
+import MoviesTable from './TableComponents/TestBookingsTableComponent'
 
 function TestComponent() {
   return (
     <div className='home-container'>
-      <AddTheatreComponent/>
+      {/* <MoviesTable/> */}
     </div>
   )
 }
